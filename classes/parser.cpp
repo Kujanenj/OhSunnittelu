6c519a4462a8c0602810ae0b78f9c099dd3212d1
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-01-30 14:25:45 +0200

    lisätty base classi parserille

---
+++ classes/parser.cpp
@@ -0,0 +1,21 @@
+#include "parser.h"
+
+
+/*!
+ * \brief Parser::Parser Vaatii seuraavia asioita:
+ * Kaikkea
+ * Lue tiedostostosta?
+ * filtteröi kaikki paska
+ * Tallenna johonki? Vai lähetä suoraa mainille? (Tai managerille tai jonnekki)
+ * Funktiota ->
+ * read file
+ * parse
+ * error checkit
+ * kirjoita logiin? vai lähetä error viestit mainiin joka kirjoittaa ne logii?
+ *
+ *
+ */
+Parser::Parser()
+{
+
+}
