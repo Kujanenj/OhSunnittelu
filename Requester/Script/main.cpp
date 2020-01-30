c242085e52b943e6b92b4326cf7c7bb66c8d5dfb
Veikko Hiltunen <veikko.hiltunen@tuni.fi>
2020-01-30 13:25:41 +0200

    Python script on C++

---
+++ Requester/Script/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <QStringList>
+#include <QProcess>
+#include <QDebug>
+#include <QFile>
+using namespace std;
+
+int main()
+{
+    QStringList arguments {"../../Python_Script/main.py"};
+    QProcess p;
+    p.start("python", arguments);
+     QString p_stdout = p.readAllStandardOutput();
+
+    p.waitForFinished();
+
+
+        QString filename = "errors.txt";
+        QFile file(filename);
+        if (file.open(QIODevice::ReadWrite)) {
+            QTextStream stream(&file);
+            stream<<"Exit code "<<p.exitCode()<<endl
+            <<p.readAllStandardError();
+            qDebug() << p.readAllStandardError() << p.readAllStandardOutput();
+
+        }
+        qDebug()<<"Done";
+
+}
