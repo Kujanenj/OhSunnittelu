251c17201ef652f6470bb3ae62f08fe25eaa2728
Veikko Hiltunen <veikko.hiltunen@tuni.fi>
2020-02-05 12:55:41 +0200

    Scripti käy läpi mapin jossa element_namet + valuet

--- Python_Script/main.py
+++ Python_Script/main.py
@@ -4,28 +4,39 @@ from selenium import webdriver
 
 # import time
 
-#driver = webdriver.PhantomJS("E:\Gitin repo\ohSuunnittelu\OhSunnittelu\Python_Script\phantomjs.exe") # WINDOWS VERSIO - Laita polku phantomjs.exe filuun
-driver = webdriver.PhantomJS() # LINUX VERSIO
+# Veikon polku: D:\Git Veikko\OhSunnittelu\Python_Script\phantomjs.exe
+# Jonkun polku: E:\Gitin repo\ohSuunnittelu\OhSunnittelu\Python_Script\phantomjs.exe
+
+driver = webdriver.PhantomJS("D:\Git Veikko\OhSunnittelu\Python_Script\phantomjs.exe") # WINDOWS VERSIO - Laita polku phantomjs.exe filuun
+#driver = webdriver.PhantomJS() # LINUX VERSIO
 
 driver.get("https://www.finlandiahiihto.fi/Tulokset/Tulosarkisto") # Etsii tulosarkiston
 
 
-# string vuosi = "dnn$ctr1025$Etusivu$ddlVuosi2x"
-# string matka = "dnn$ctr1025$Etusivu$ddlMatka2x"
-# string sukupuoli = "dnn$ctr1025$Etusivu$chkLstSukupuoli2"
-# string ikaluokka = "dnn$ctr1025$Etusivu$ddlIkaluokka2"
-# string etunimi = "dnn$ctr1025$Etusivu$txtHakuEtunimi2"
-# string sukunimi = "dnn$ctr1025$Etusivu$txtHakuSukunimi2"
-# string paikkakunta = "dnn$ctr1025$Etusivu$txtHakuPaikkakunta2"
-# string kansallisuus = "dnn$ctr1025$Etusivu$ddlKansalaisuus2x"
-# string joukkue = "dnn$ctr1025$Etusivu$txtHakuJoukkue2"
+# Sukupuoli valuet: kaikki, M, N
+# Ikäsarja valuet: kaikki, alle35, 35, 40, 45, 50, 55, 60, 65, 70, 75, yli80
+# Matka valuet: kaikki, P50, V50, P100, P32, V20, V32, V20jun, P42, V42, V32, P20,
+#               P30, P44, P60, P62, P25, P32, P35, P45, P52, P53, P75, V30, V45, V53, V75
+# Vuosi valuet: kaikki, 2019-1974 yksittäin
 
-# 5 Elementtiä, johon input value/radio button
-#for x in range(0, 4):
-#    driver.find_element_by_name("key").sendKeys("value")
-# 4 Elementtiä, johon dropdown valikko
-#for y in range(0, 3):
-#    driver.find_element_by_name("key").selectByValue("value")
+
+
+# Dictissä 4 ensimmäistä drop down valikkoja, loput inputteja
+dict = {
+    "dnn$ctr1025$Etusivu$ddlVuosi2x" : "2019",
+    "dnn$ctr1025$Etusivu$ddlMatka2x" : "P50",
+    "dnn$ctr1025$Etusivu$ddlIkaluokka2" : "kaikki",
+    "dnn$ctr1025$Etusivu$ddlKansalaisuus2x" : "",
+    "dnn$ctr1025$Etusivu$chkLstSukupuoli2" : "kaikki",
+    "dnn$ctr1025$Etusivu$txtHakuEtunimi2" : "Veikko",
+    "dnn$ctr1025$Etusivu$txtHakuSukunimi2" : "",
+    "dnn$ctr1025$Etusivu$txtHakuPaikkakunta2" : "",
+    "dnn$ctr1025$Etusivu$txtHakuJoukkue2" : ""
+}
+
+# 8 Elementtiä
+for x in range(0, 8):
+    driver.find_element_by_name(list(dict.keys())[x]).send_keys(list(dict.values())[x])
 
 
 
