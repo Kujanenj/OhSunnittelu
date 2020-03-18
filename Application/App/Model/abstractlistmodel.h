40d6e3a245328bbf99ec88a9fcd0eb3446f28390
Veikko Hiltunen <veikko.hiltunen@tuni.fi>
2020-03-18 15:56:40 +0200

    Commented abstractlistmodel

--- Application/App/Model/abstractlistmodel.h
+++ Application/App/Model/abstractlistmodel.h
@@ -7,13 +7,48 @@ class AbstractListModel : public QSqlQueryModel
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief AbstractListModel
+     * Constructor for AbstractListModel
+     * @param parent
+     */
     AbstractListModel(QObject *parent = nullptr);
 
+    /**
+     * @brief data
+     * Used on background to update specific role on specific
+     * row to database
+     * @param index
+     * @param role
+     * @return
+     */
     virtual QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+
+    /**
+     * @brief sortModel
+     * Used to sort data on sql
+     * @param command : contains command to data that must be sorted
+     */
     virtual void sortModel(QString command);
+
 public slots:
+
+    /**
+     * @brief updateModel
+     * Updates specific table
+     * @param mainTable : table to update
+     */
     void updateModel(QString mainTable);
+
 protected:
+
+    /**
+     * @brief roleNames
+     * Pure-virtual method that is used on derived
+     * classes to set roles for qml
+     * @return
+     */
     virtual QHash<int, QByteArray> roleNames() const = 0;
 };
 
